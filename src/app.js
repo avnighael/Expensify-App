e034f3a664afb2d251c30254d6bb889644fb237b
avnighael <avi.ghael@gmail.com>
2018-07-23 16:27:47 -0400

    HOC and started connecting store and component with react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
@@ -16,10 +17,14 @@ store.dispatch(addExpense({ description: 'Gas bill', amount:100, createdAt: 1000
 // console.log(store.getState());
 store.dispatch(setTextFilter('bill'));
 
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
